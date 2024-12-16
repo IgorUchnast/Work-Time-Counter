d9d027f824752865f89e91d24dbd73e0a617e8c6
Igor Uchnast <igoruchnast@MacBook-Pro-Igor.local>
2024-12-16 14:52:29 +0100

    deleeted .venv, ,vscode

--- llama_app/test_github.py
+++ llama_app/test_github.py
@@ -22,13 +22,26 @@ def get_repo_tree(owner, repo):
     else:
         print(f"Błąd: {response.status_code} - {response.json()}")
         return []
-print(get_repo_tree(OWNER, REPO))
+
+def get_file(owner, repo, file_name):
+    """
+    Pobiera plik .gitignore z repozytorium.
+    """
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_name}"
+    # url = f"https://api.github.com/repos/{owner}/{repo}/contents/.gitignore"
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code == 200:
+        file_content = response.json()["content"]
+        decoded_content = base64.b64decode(file_content).decode("utf-8")
+        return decoded_content.splitlines()
+    else:
+        print(f"Błąd pobierania pliku .gitignore: {response.status_code}")
+        return []
 
 def download_file(owner, repo, path, save_path):
     """
     Pobiera zawartość pliku i zapisuje go lokalnie.
     """
-    # print()
     url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
     response = requests.get(url, headers=HEADERS)
     if response.status_code == 200:
@@ -43,20 +56,40 @@ def download_file(owner, repo, path, save_path):
     else:
         print(f"Błąd pobierania pliku {path}: {response.status_code}")
 
+def remove_hash_elements(arr):
+    return [item for item in arr if "#" not in item]
+
+
 def download_python_files(owner, repo):
     """
-    Pobiera wszystkie pliki .py oraz zapisuje foldery, które je zawierają.
+    Pobiera wszystkie pliki .py oraz zapisuje foldery, które je zawierają,
+    pomijając foldery/plik w .gitignore.
     """
+    # Pobierz zawartość .gitignore
+    gitignore_patterns = get_file(owner, repo, '.gitignore')
+    git_ignore_paths = remove_hash_elements(gitignore_patterns)
+    print(git_ignore_paths)
     tree = get_repo_tree(owner, repo)
+
+    # Filtrowanie i pobieranie plików
     for item in tree:
-        print(item["path"])
-        # Sprawdzamy tylko pliki z rozszerzeniem .py
+        # Sprawdzamy tylko pliki .py
         if item["type"] == "blob" and item["path"].endswith(".py"):
-            file_path = item["path"]
-            save_path = os.path.join(repo, file_path)
-            download_file(owner, repo, file_path, save_path)
+            ignore = False
+            head =  "/".join(item["path"].split("/")[:2])
+            print(head)
+            for path in git_ignore_paths:
+                print(path)
+                if f"{head}/" == f"flask_app/{path}":
+                    ignore = True
+                elif path != f"flask_app/{path}":
+                    print("**********************")
+                    print(head)
+                    print(f"flask_app/{path}")
+                    file_path = item["path"]
+                    save_path = os.path.join(repo, file_path)
+                    download_file(owner, repo, file_path, save_path)
 
 if __name__ == "__main__":
-    # Pobierz wszystkie pliki .py z repozytorium
+    # Pobierz wszystkie pliki .py z repozytorium, pomijając te w .gitignore
     download_python_files(OWNER, REPO)
-
