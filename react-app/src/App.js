4a38e635ce44c474412a913024bd2169c313df4e
Bartosz Kurkus <bartejos@gmail.com>
2024-12-18 03:02:47 +0100

    Update App and index

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard type={null}/>} />
+        <Route path='/employee/:employee_id' element={<Dashboard type='employees'/>} />
+        <Route path='/project/:project_id' element={<Dashboard type='projects'/>} />
+        {/* <Route path='*' element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
