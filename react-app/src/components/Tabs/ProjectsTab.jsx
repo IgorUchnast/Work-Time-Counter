203822d4980da87fe5f865acdf67de7bb4285d3d
Bartosz Kurkus <bartejos@gmail.com>
2025-01-27 21:53:09 +0100

    String renaming

--- react-app/src/components/Tabs/ProjectsTab.jsx
+++ react-app/src/components/Tabs/ProjectsTab.jsx
@@ -93,8 +93,8 @@ export const EmployeeProjectTaskAssignments = ({ employee_id, project_id }) => {
                             <strong>{assignment.assignment_name}</strong> <i>({assignment.status})</i><br/>
                             <strong>Id zadania: </strong>{assignment.assignment_id}<br/>
                             <strong>Opis: </strong>{assignment.description}<br/>
-                            <strong>Data rozpoczęcia: </strong>{assignment.start_date ? (assignment.start_date) : ("brak")}<br/>
-                            <strong>Data zakończenia: </strong>{assignment.stop_date ? (assignment.stop_date) : ("brak")}
+                            <strong>Data ostatniego rozpoczęcia: </strong>{assignment.start_date ? (assignment.start_date) : ("brak")}<br/>
+                            <strong>Data ostatniego zakończenia: </strong>{assignment.stop_date ? (assignment.stop_date) : ("brak")}
                         </ListGroup.Item>
                     </div>
                 ))}
